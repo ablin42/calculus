67893dd533a349782de3d09fdc079e1b0a1bb07a
ablin <space6fic@gmail.com>
2019-12-23 18:39:19 +0100

    refactored calculator, made class for security and flexibily purposes, changed calculator display from table to grid, still handling keyboard, better calculation handling, operator limited to 1

--- public/script.js
+++ public/script.js
@@ -1,81 +1,157 @@
-window.addEventListener("keyup", function(event) {
-    let keyPressed = event.keyCode;
-    if (keyPressed >= 96 && keyPressed <= 111)
-        insert(event.key);
-    if (keyPressed === 13)
-        equal();
-    if (keyPressed === 8)
-        back();
+class Calculator {
+    constructor(previousOperandText, currentOperandText) {
+        this.previousOperandText = previousOperandText;
+        this.currentOperandText = currentOperandText;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number) {
+        let currOperandString = this.currentOperand.toString();
+
+        if (number === "." && currOperandString.includes('.')) return;
+        this.currentOperand = currOperandString + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === "") return;
+        if (this.previousOperand !== "") this.compute();
+
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand);
+        const curr = parseFloat(this.currentOperand);
+
+        if (isNaN(prev) || isNaN(curr)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + curr;
+                break;
+            case '-':
+                computation = prev - curr;
+                break;
+            case '*':
+                computation = prev * curr;
+                break;
+            case '÷':
+                computation = prev / curr;
+                break;   
+            default:
+                return;     
+        }
+        this.currentOperand = computation;
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+
+        if (isNaN(integerDigits))
+            integerDisplay = "";
+        else 
+            integerDisplay = integerDigits.toLocaleString('fr', {maximumFractionDigits: 0});
+        if (decimalDigits != null) 
+            return `${integerDisplay}.${decimalDigits}`;
+        else 
+            return integerDisplay;
+    }
+
+    updateDisplay() {
+        this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operation != null) 
+            this.previousOperandText.innerText = `${this.previousOperand} ${this.operation}`;
+        else 
+            this.previousOperandText.innerText = "";
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandText = document.querySelector('[data-previous]');
+const currentOperandText = document.querySelector('[data-current]');
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
 })
 
-let textview = document.getElementById("textview");
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
 
-function evil(exp) { /* caqlc func  (eval alternative) */
-    return new Function('return ' + exp)();
-}
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
 
-function calculator(exp) {
-    return ;
-}
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
 
-function equal(){
-    try {
-        var exp = document.getElementById("textview").value;
-        if (exp){
-            document.getElementById("textview").value = evil(exp);
-            //document.getElementById("textview").value = eval(exp);
-        }
-    } catch (err) {
-        console.log("An error occured:", err.message);
-        clean();
-        var alert = `
-                <div id="alert" class="alert" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true" onclick="dismissAlert(this)">x</span></button>
-                    <b>${err.message}</b>
-                </div>`;
-        addAlert(alert, "#header")   
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
+
+
+window.addEventListener("keyup", function(event) {
+    let keyPressed = event.keyCode;
+    let key = event.key;
+
+    if ((keyPressed >= 96 && keyPressed <= 105) || keyPressed === 110) {
+        calculator.appendNumber(key);
+        calculator.updateDisplay();
     }
-}
-
-function insert(num){
-    document.getElementById("textview").value = document.getElementById("textview").value + num;
-}
-
-function clean(){
-    document.getElementById("textview").value = "";
-}
-
-function back(){
-    var exp = document.getElementById("textview").value;
-    document.getElementById("textview").value = exp.substring(0,(exp.length - 1));
-}
-
-function dismissAlert(closeBtn)
-{
-    var alert = closeBtn.parentElement,
-    wrap = document.getElementById("alertwrapper");
-    if (wrap)
-        wrap.remove();
-    alert.remove();
-}
-
-function addAlert(alert, where)
-{
-    let node = document.createElement('div'),
-        alertDiv = document.getElementById("alert"),
-        wrap = document.getElementById("alertwrapper");
-
-    node.setAttribute("id", "alertwrapper");
-    if (alertDiv){
-        if (wrap) 
-            wrap.remove();
-        alertDiv.remove();
+    if (keyPressed === 106 || keyPressed === 107 || keyPressed === 109 || keyPressed === 111) {
+        if (keyPressed === 111)
+            key = "÷";
+        calculator.chooseOperation(key);
+        calculator.updateDisplay();
     }
-    node.innerHTML += alert;
-    $(where).after(node);   
-}
+    if (keyPressed === 46) {
+        calculator.clear();
+        calculator.updateDisplay();
+    }
+    if (keyPressed === 13) {
+        calculator.compute();
+        calculator.updateDisplay();
+    }
+    if (keyPressed === 8 || keyPressed === 187) {
+        calculator.delete();
+        calculator.updateDisplay(); 
+    }
+})
 
-$('.button').on('click', function() {
+$('button').on('click', function() {
     let target = this;
     $(this).toggleClass('clicked');
     setTimeout(function(){ $(target).toggleClass('clicked') }, 250);
